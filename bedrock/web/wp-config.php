a64b89bd3e6b9b7863d63512cd8a83ce1738ca92
jesus.gonzalez <38189444+gssusss@users.noreply.github.com>
2024-01-16 08:27:03 -0500

    Update wp-config.php

--- bedrock/web/wp-config.php
+++ bedrock/web/wp-config.php
@@ -4,6 +4,10 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
+define('DB_NAME', 'mi-sitio');
+define('DB_USER', 'mi-sitio');
+define('DB_PASSWORD', 'mi-sitio');
+define('DB_HOST', 'localhost');
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
 require_once ABSPATH . 'wp-settings.php';
